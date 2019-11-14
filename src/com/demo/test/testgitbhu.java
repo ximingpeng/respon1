e8ce85b96a6e14598e5b7fe00459a7f09b17ca9f
ximingpeng <1603422829@qq.com>
2019-11-14 16:49:41 +0800

    1.0项目

--- src/com/demo/test/testgitbhu.java
+++ src/com/demo/test/testgitbhu.java
@@ -5,4 +5,9 @@ public class testgitbhu {
     public static void main(String[] args) {
         System.out.println("你好美丽");
     }
+
+    public void test(){
+        System.out.println("新添加");
+    }
+
 }
